0e4a1a4515709fcaf726e5bbfc8d6d08416b3e41
Anastasia-Arnautova <114022978+Anastasia-Arnautova@users.noreply.github.com>
2022-12-26 19:15:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <body>    
+
+    <form action="form.php" method="post">
+      <table>
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td>Input Name</td>
+            <td><input type="text" name="inputName"/></td>
+          </tr>
+          <tr>
+            <td>Input Response</td>
+            <td><textarea name="inputResponse"></textarea></td>
+          </tr>
+        </tbody>
+      </table>
+      <button type="submit">Submit</button>
+
+    </form>    
+  </body>
+</html>
